40a9b745766d806e617c96e96a688b1668b41f27
gulliish <gulmiraabdujalilova@gmail.com>
2022-06-18 20:45:04 +0600

    final commit

--- task-02/index.js
+++ task-02/index.js
@@ -1,3 +1,10 @@
-function test() {
+function test(num) {
+    const array = [2, 3, 4, 5];
+    for(let i = 0; i < array.length; i++){
+        if(num % array[i] === 0){
+            console.log(array[i]);
+        }
+    }
     
 }
+test(300);
